a18b3bd9f7bff9f58490f15fae3a5803fdd84ca9
rinya7 <v.dmitriev0712@gmail.com>
2024-04-05 14:33:17 +0200

    desctop version header and hero

--- js/main.js
+++ js/main.js
@@ -25,4 +25,76 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Здесь можно добавить код для отправки данных на сервер или другие действия
   });
+
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slider .slide");
+  const dots = document.querySelectorAll(".dots .dot");
+
+  const slideWidth = 170; // Ширина одного слайда
+  const slideMargin = 10; // Внешний отступ между слайдами
+  const slidesPerRow = 4; // Количество слайдов в строке
+  const totalSlides = slides.length; // Общее количество слайдов
+  let currentIndex = slidesPerRow;
+
+  // Добавляем виртуальные слайды в начало и конец
+  function addVirtualSlides() {
+    for (let i = 0; i < slidesPerRow; i++) {
+      // Добавляем по 4 виртуальных слайда в начало
+      const cloneFirst = slides[i].cloneNode(true);
+      const cloneLast = slides[totalSlides - 1 - i].cloneNode(true);
+      slider.appendChild(cloneFirst);
+      slider.insertBefore(cloneLast, slider.firstChild);
+    }
+    slider.style.transition = "none";
+    slider.style.transform = `translateX(-${
+      (slideWidth + slideMargin) * currentIndex
+    }px)`;
+  }
+
+  addVirtualSlides();
+
+  function scrollToSlide(index) {
+    const offset = (slideWidth + slideMargin) * index;
+    slider.style.transition = "transform 0.3s ease-in-out";
+    slider.style.transform = `translateX(-${offset}px)`;
+    currentIndex = index;
+    updateActiveDot();
+  }
+
+  function updateActiveDot() {
+    dots.forEach((dot, index) => {
+      dot.classList.toggle(
+        "active",
+        index === currentIndex - (slidesPerRow - 1)
+      );
+    });
+  }
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      scrollToSlide(index + slidesPerRow);
+    });
+  });
+
+  slider.addEventListener("transitionend", () => {
+    if (currentIndex <= slidesPerRow - 1) {
+      currentIndex = totalSlides + currentIndex;
+      slider.style.transition = "none";
+      slider.style.transform = `translateX(-${
+        (slideWidth + slideMargin) * currentIndex
+      }px)`;
+    } else if (currentIndex >= totalSlides + slidesPerRow) {
+      currentIndex = currentIndex - totalSlides;
+      slider.style.transition = "none";
+      slider.style.transform = `translateX(-${
+        (slideWidth + slideMargin) * currentIndex
+      }px)`;
+    }
+  });
+
+  function nextSlide() {
+    scrollToSlide(currentIndex + 1);
+  }
+
+  setInterval(nextSlide, 3000);
 });
